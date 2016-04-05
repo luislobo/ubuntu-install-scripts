ac1623a87630c3eb84fd30bb3a8a647dfb82473d
Luis Lobo Borobia <luislobo@gmail.com>
2016-04-05 14:47:27 -0500

    Added Control-C support, and removed sudo from update

--- installsw-15.10.sh
+++ installsw-15.10.sh
@@ -5,6 +5,15 @@ if [[ $EUID -eq 0 ]]; then
   exit 1
 fi
 
+control_c()
+# run if user hits control-c
+{
+  echo -en "\n*** Ouch! Exiting ***\n"
+  exit $?
+}
+# trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
 ##### INSTALL CLI PACKAGES ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
 # Java
--- update-15.10.sh
+++ update-15.10.sh
@@ -10,55 +10,70 @@ if [[ $EUID -ne 0 ]]; then
     exit;
 fi
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+control_c()
+# run if user hits control-c
+{
+  echo -en "\n*** Ouch! Exiting ***\n"
+  exit $?
+}
+# trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
+##### UPDATE ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
 
 # python-software-properties installs add-apt-repository
-sudo apt-get install -y python-software-properties
+apt-get install -y python-software-properties
 
-##### REPOSITORIES ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
+##### REPOSITORIES ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
 
 # Git
-sudo add-apt-repository -y ppa:git-core/ppa
+add-apt-repository -y ppa:git-core/ppa
 
 # Java 8
-sudo add-apt-repository -y ppa:webupd8team/java
+add-apt-repository -y ppa:webupd8team/java
 
 # Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 
 # Nginx
-sudo add-apt-repository -y ppa:nginx/development
+add-apt-repository -y ppa:nginx/development
 
 # MongoDB 3.2
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 ### SPECIFIC STUFF LUIS USES
 
 # spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
+
+# Maria DB
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+add-apt-repository 'deb [arch=amd64,i386] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu wily main'
 
 # Intel graphics card key
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A902DDA375E52366
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A902DDA375E52366
 
 # Various, in specific, truecrypt
-sudo add-apt-repository -y ppa:stefansundin/truecrypt
+add-apt-repository -y ppa:stefansundin/truecrypt
 
 # Clementine
-sudo add-apt-repository -y ppa:me-davidsansome/clementine-dev
+add-apt-repository -y ppa:me-davidsansome/clementine-dev
 
 # Equalizer
-sudo add-apt-repository -y ppa:nilarimogard/webupd8
+add-apt-repository -y ppa:nilarimogard/webupd8
 
 # Libre Office
-sudo add-apt-repository -y ppa:libreoffice/ppa
+add-apt-repository -y ppa:libreoffice/ppa
 
 # Wine
-sudo add-apt-repository -y ppa:ubuntu-wine/ppa
+add-apt-repository -y ppa:ubuntu-wine/ppa
 
 ##### UPDATE REPOSITORIES ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
-sudo apt-get update
+apt-get update
