8e764f97e0a7318148238f103677d0507232dc1b
Luis Lobo Borobia <luislobo@gmail.com>
2016-04-05 14:55:15 -0500

    several changes

--- installsw-15.10.sh
+++ installsw-15.10.sh
@@ -14,124 +14,181 @@ control_c()
 # trap keyboard interrupt (control-c)
 trap control_c SIGINT
 
+### URLs. Get them from their sources
+
+# https://www.vagrantup.com/downloads.html
+vagrant_deb="https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb"
+
+# https://github.com/creationix/nvm#install-script
+nvm_url="https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh"
+
+# https://github.com/robbyrussell/oh-my-zsh#via-wget
+oh_my_zsh_url="https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
+
+# https://netbeans.org/downloads/start.html?platform=linux&lang=en&option=html&bits=x64
+netbeans_url="http://download.netbeans.org/netbeans/8.1/final/bundles/netbeans-8.1-html-linux-x64.sh"
+
+##### SETUP FUNCTIONS ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
+
+export DEBIAN_FRONTEND=noninteractive
+
+function installFromUrl {
+  wget --quiet --output-document=- $1 | sudo dpkg --install -
+}
+
+function aptGetInstall {
+  sudo apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
+}
+
+function aptGetRemove {
+  sudo apt-get remove -y "$@"
+}
+
 ##### INSTALL CLI PACKAGES ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### 
 
+
 # Java
+# remove old java
+aptGetRemove openjdk-7-jre default-jre openjdk-7-jre default-jre-headless openjdk-7-jre-headless
+
 # Auto accept oracle installer license
-sudo apt-get install -y oracle-java8-installer
-sudo update-java-alternatives -s java-8-oracle
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+  sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+  sudo debconf-set-selections
+
+aptGetInstall oracle-java8-installer oracle-java8-set-default
+#sudo update-java-alternatives -s java-8-oracle
 
 # Very basic Linux build tools
-sudo apt-get -y install build-essential make
+aptGetInstall build-essential make
 
 # ZSH, OhMyZSH, its plugins & set as default for current user
-sudo apt-get install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i 's/plugins=(git)/plugins=(git command-not-found common-aliases compleat fabric mercurial npm pip python redis-cli vagrant yii)/g' ~/.zshrc
+aptGetInstall zsh
+sh -c "$(curl -fsSL $oh_my_zsh_url)"
+sed -i 's/plugins=(git)/plugins=(aws bower command-not-found composer common-aliases compleat debian dircycle dirhistory dirpersist docker gitfast git-extras fabric last-working-dir mercurial node npm pip python red
+is-cli screen sublime sudo systemd vagrant web-search yii yii2 wd)/g' ~/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="amuse"/g' ~/.zshrc
 
 # Basic Common tools
-sudo apt-get install -y mc htop curl ntp ntfs-config
+aptGetInstall mc htop curl ntp ntfs-config
 
 # Compression tools
-sudo apt-get install -y p7zip-full unrar
+aptGetInstall p7zip-full unrar
 
 # Git and tig
-sudo apt-get install -y git git-cola tig
+aptGetInstall git git-cola tig
 
 # Mercurial
-sudo apt-get install -y mercurial
+aptGetInstall mercurial
 
 # Byobu and sets byobu as an available shell
-sudo apt-get install -y byobu
+aptGetInstall byobu
 BYOBU_LOCATION=$(which byobu)
 sudo sh -c "grep -q '$BYOBU_LOCATION' /etc/shells || echo '$BYOBU_LOCATION'>> /etc/shells"
 
 # nvm, node and modules
-bash -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh"
-nvm install 4.3
-nvm use 4.3
-nvm alias default 4.3
+bash <(wget -qO- $nvm_url)
+export NVM_DIR="$HOME/.nvm"
+echo $NVM_DIR
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install 4
+nvm use 4
+nvm alias default 4
+which node
+which npm
+node --version
+npm --version
+npm install npm@latest -g
+npm --version
 
 # NPM modules
-sudo npm install -g sails bower grunt-cli pm2 mocha jshint express-generator gulp karma coffee-script dependency-check node-inspector
+npm install -g sails bower grunt-cli pm2 mocha jshint express-generator gulp karma coffee-script dependency-check node-inspector npm-check-updates
 
 # Audio and Video avconv & other video tools (replaces ffmpeg)
-sudo apt-get install -y ffmpeg
+aptGetInstall ffmpeg
 
 # Test disk (recovery tools)
-sudo apt-get install -y testdisk
+aptGetInstall testdisk
 
 # iostat
-sudo apt-get install -y sysstat
+aptGetInstall sysstat
 
 # iotop
-sudo apt-get install -y iotop
+aptGetInstall iotop
 
 # nethogs - View network and processes
-sudo apt-get install -y nethogs
+aptGetInstall nethogs
 
 # network TOP
-sudo apt-get install -y iftop
+aptGetInstall iftop
 
 # whois
-sudo apt-get install -y whois
+aptGetInstall whois
 
 # Install preload (speeds up stuff)
-sudo apt-get install -y preload
+aptGetInstall preload
 
 # Enhances Notebook Batery Life
-sudo apt-get install -y tlp tlp-rdw
+aptGetInstall tlp tlp-rdw
 sudo tlp start
 
 # Nginx
-sudo apt-get install -y nginx
+aptGetInstall nginx
 
 # MongoDB
-sudo apt-get install -y libkrb5-dev
-sudo apt-get install -y mongodb-org
+aptGetInstall libkrb5-dev
+aptGetInstall mongodb-org
+
+# MariaDB
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password PASS'
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password PASS'
+aptGetInstall mariadb-server
+mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('123');"
 
 # Redis
-sudo apt-get install -y redis-server
+aptGetInstall redis-server
 
 # SQLite
-sudo apt-get install -y sqlite
+aptGetInstall sqlite
 
 # Memcached
-sudo apt-get install -y memcached
+aptGetInstall memcached
 
 # Encrypt FS utils
-sudo apt-get install -y ecryptfs-utils
+aptGetInstall ecryptfs-utils
 
 # Gnome Partition Editor
-sudo apt-get install -y gparted
+aptGetInstall gparted
 
 # Fabric tool
-sudo apt-get install -y fabric
+aptGetInstall fabric
 
 # DKMS - updates things like virtualbox when a new kernel appears
-sudo apt-get install -y dkms
+aptGetInstall dkms
 
 # Linux containers
-sudo apt-get install -y lxc lxctl lxc-templates
+aptGetInstall lxc lxctl lxc-templates
+
+# Vagrant
+sudo installFromUrl $vagrant_deb
 
 # Better Deb installer
-sudo apt-get install -y gdebi
-
+aptGetInstall gdebi
 
 ##### GUI PACKAGES ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
 
 # Anonymous PRO font
-sudo apt-get install -y ttf-anonymous-pro
+aptGetInstall ttf-anonymous-pro
 
 # TortoiseHG
-sudo apt-get install -y tortoisehg
+aptGetInstall tortoisehg
 
 # Meld
-sudo apt-get install -y meld
+aptGetInstall meld
 
 # Guake
-sudo apt-get install -y guake
+aptGetInstall guake
 
 # sets
 # default shell to byobu
@@ -208,43 +265,36 @@ gconftool-2 --load guake.xml
 rm -f guake.xml
 
 # Virtualbox
-#sudo apt-get install -y virtualbox virtualbox-guest-additions-iso
+aptGetInstall virtualbox virtualbox-guest-additions-iso
 
 # Chrome
-sudo apt-get install -y google-chrome-stable
+aptGetInstall google-chrome-stable
 
 # Unity Tweak tool
-sudo apt-get install -y unity-tweak-tool
+aptGetInstall unity-tweak-tool
 
 # Indicator Multiload
-sudo apt-get install -y indicator-multiload
+aptGetInstall indicator-multiload
 
 ### LUIS SPECIFIC STUFF
 
 # spotify
-sudo apt-get install -y spotify-client
+aptGetInstall spotify-client
 
 # zsync
-sudo apt-get install -y zsync
+aptGetInstall zsync
 
 # Screen profiles
-sudo apt-get install -y arandr
+aptGetInstall arandr
 
 # Hardware info
-sudo apt-get install -y hwinfo
+aptGetInstall hwinfo
 
 # Install Atom Editor
-wget https://atom.io/download/deb -O atom.deb
-sudo dpkg -i atom.deb
-rm -y atom-deb
-
-# MySQL
-#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-#sudo apt-get install -y mysql-server php5-dev php5-cli php5-gd php5-fpm php5-mcrypt php5-json php5-mysql php5-xdebug php5-geoip php5-memcached php5-xmlrpc php5-xsl
+installFromUrl https://atom.io/download/deb
 
 # Openvpn client
-sudo apt-get install -y openvpn network-manager-openvpn
+aptGetInstall openvpn network-manager-openvpn
 
 # adds keep alive to ssh session
 # mkdir ~/.ssh
@@ -252,13 +302,13 @@ sudo apt-get install -y openvpn network-manager-openvpn
 # grep -q 'ServerAliveInterval' .ssh/config || sed -i '1iHost *\n\tServerAliveInterval 300\n\tServerAliveCountMax 2\n\n' ~/.ssh/config
 
 # Better volume control (have to run it manually as it does not replace built-in)
-sudo apt-get install -y pavucontrol
+aptGetInstall pavucontrol
 
 # WM ctrl to send messages to windows
-sudo apt-get install -y wmctrl
+aptGetInstall wmctrl
 
 # Samba
-sudo apt-get install -y samba
+aptGetInstall samba
 
 # Intel drivers for Ubuntu 14.04 64 bit
 #wget https://download.01.org/gfx/ubuntu/14.04/main/pool/main/i/intel-linux-graphics-installer/intel-linux-graphics-installer_1.0.5-0intel1_amd64.deb
@@ -266,19 +316,19 @@ sudo apt-get install -y samba
 #rm intel-linux-graphics-installer_1.0.5-0intel1_amd64.deb
 
 # Pdf MOD
-sudo apt-get install -y pdfmod
+aptGetInstall pdfmod
 
 # Logitech Unified Receiver Manager
-sudo apt-get install -y solaar
+aptGetInstall solaar
 
 # Pulse audio equalizer
-sudo apt-get install -y pulseaudio-equalizer
+aptGetInstall pulseaudio-equalizer
 
 # GStreamer Plugin for wma in Clementine
-sudo apt-get install -y gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse
+aptGetInstall gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse
 
 # Compiz
-sudo apt-get install -y compizconfig-settings-manager
+aptGetInstall compizconfig-settings-manager
 
 # use compiz settings manager and change this settings
 # Launcher Reveal Pressure = 3
@@ -288,52 +338,52 @@ sudo apt-get install -y compizconfig-settings-manager
 # Sync to VBlank = false
 
 # Advanced volume control
-sudo apt-get install -y pavucontrol
+aptGetInstall pavucontrol
 
 # skype
 # First, libraries so that skype looks correctly on Ubuntu 64
-sudo apt-get install -y gtk2-engines-murrine:i386 gtk2-engines-pixbuf:i386
-sudo apt-get install -y skype
+aptGetInstall gtk2-engines-murrine:i386 gtk2-engines-pixbuf:i386
+aptGetInstall skype
 
 # Truecrypt
-sudo apt-get install -y truecrypt
+aptGetInstall truecrypt
 
 # Record my desktop
-sudo apt-get install -y gtk-recordmydesktop
+aptGetInstall gtk-recordmydesktop
 
 # Clementine
-sudo apt-get install -y clementine
+aptGetInstall clementine
 
 # VLC
-sudo apt-get install -y vlc
+aptGetInstall vlc
 
 # GIMP
-sudo apt-get install -y gimp gimp-plugin-registry
+aptGetInstall gimp gimp-plugin-registry
 
 # Feh
-sudo apt-get install -y feh
+aptGetInstall feh
 
 # Wine
-sudo apt-get install -y wine winetricks
+aptGetInstall wine winetricks
 
 # remove fonts-unfonts-core because it makes java render funny
-sudo apt-get remove -y fonts-unfonts-core
+aptGetRemove -y fonts-unfonts-core
 
 # install netbeans
-wget http://download.netbeans.org/netbeans/8.1/final/bundles/netbeans-8.1-html-linux-x64.sh -O latest-netbeans.sh
+wget $netbeans_url -O latest-netbeans.sh
 sudo sh latest-netbeans.sh
 rm -f latest-netbeans.sh
 
 # install exfat format. Needed for SD cards used in phones
-sudo apt-get install -y exfat-fuse exfat-utils
+aptGetInstall exfat-fuse exfat-utils
 
 # remove zeitgeist
-sudo apt-get remove -y zeitgeist-core
+aptGetRemove zeitgeist-core
 
 # remove unwanted unity-lens
-sudo apt-get remove -y unity-lens-video unity-lens-photos unity-lens-music
+aptGetRemove unity-lens-video unity-lens-photos unity-lens-music
 
 # remove apport
-sudo apt-get remove -y apport-symptoms apport
+aptGetRemove apport-symptoms apport
 
-sudo apt-get autoremove -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
